c5bf123c0f59fc1bfc3d66b63894086477ddacc4
Bogdan Radu <radu.bogdan17@gmail.com>
2022-01-16 01:59:31 +0100

    added test console

---
+++ Interceptor/TestConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TestObject();
+
+            test.SimpleMethod();
+
+            test.ParameterMethod(2);
+
+            var n = test.SimpleFunction();
+            Console.WriteLine($"Return {n}");
+
+            try
+            {
+                test.ExceptionMethod();
+            }
+            catch(Exception)
+            {
+                //
+            }
+        }
+    }
+}
---
+++ Interceptor/TestConsole/TestObject.cs
@@ -0,0 +1,36 @@
+﻿using Interceptor;
+using System;
+
+namespace TestConsole
+{
+    [Intercept]
+    public class TestObject
+    {
+        public TestObject()
+        {
+
+        }
+
+        public void SimpleMethod()
+        {
+            Console.WriteLine("SimpleMethod");
+        }
+
+        public void ExceptionMethod()
+        {
+            Console.WriteLine("ExceptionMethod");
+            throw new Exception();
+        }
+
+        public void ParameterMethod(int n)
+        {
+            Console.WriteLine($"ParameterMethod({n})");
+        }
+
+        public int SimpleFunction()
+        {
+            Console.WriteLine("SimpleFunction");
+            return 1;
+        }
+    }
+}
