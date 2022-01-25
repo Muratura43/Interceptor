a08e514f76bd500d50954ed592e0ad590dcc06c0
Bogdan Radu <radu.bogdan17@gmail.com>
2022-01-25 17:07:15 +0100

    added nuspec

--- Interceptor/Interceptor/Properties/AssemblyInfo.cs
+++ Interceptor/Interceptor/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Interceptor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A method interceptor that uses ContextBoundObject")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Buggy Tech")]
 [assembly: AssemblyProduct("Interceptor")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © Buggy Tech 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
